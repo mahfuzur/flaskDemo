6aadd16a0f8ce7b7cb88ee21b918f53edbd4ec4c
mahfuzur <mahfuzur@mahfuzurs-MacBook-Air.local>
2023-06-19 14:35:07 +0600

    add db config

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+from sqlalchemy import text
+
+from app.config.db import configure_database, db
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+
+# init db
+configure_database(app)
+
+
+def execute_raw_query(query):
+    with db.engine.connect() as connection:
+        statement = text(query)
+        result = connection.execute(statement)
+        return result.fetchall()
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, flaskDemo!</p>"
+    q = 'SELECT 10+10'
+    results = execute_raw_query(q)
+
+    print(results)
+    return '<p>Hello, flaskDemo!</p>'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ app/config/db.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+
+
+def configure_database(app):
+    # MySQL configuration values
+    db_connection = os.getenv('DB_CONNECTION')
+    db_host = os.getenv('DB_HOST')
+    db_port = os.getenv('DB_PORT')
+    db_database = os.getenv('DB_DATABASE')
+    db_username = os.getenv('DB_USERNAME')
+    db_password = os.getenv('DB_PASSWORD')
+
+    # Create the MySQL connection URL
+    db_url = f"{db_connection}://{db_username}:{db_password}@{db_host}:{db_port}/{db_database}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
